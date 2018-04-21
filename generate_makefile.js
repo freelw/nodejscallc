6c103f673afc2ad22edfbeff702afc45f5ae47cd
李望 <liwang@liwangdeMacBook-Pro.local>
2018-04-21 13:45:46 +0800

    link done

--- generate_makefile.js
+++ generate_makefile.js
@@ -4,17 +4,18 @@ return `DIR_INC = -I./
 DIR_LIB = -L./
 TARGET	= ../release/${func_name}
 CFLAGS = -g -Wall $(DIR_INC) $(DIR_LIB)
-LDFLAGS +=
+LDFLAGS += -lstdc++
 SRCDIR:=
 SRCS := $(wildcard *.cpp) $(wildcard $(addsuffix /*.cpp, $(SRCDIR)))
 OBJECTS := $(patsubst %.c,%.o,$(SRCS))
 $(TARGET) : $(OBJECTS)
 \tgcc $(CFLAGS) $^ -o $@ $(LDFLAGS)
+\tcp ../js/${func_name}_proxy.js ../release/
 %.o : %.c
 \tgcc -c $(CFLAGS) $< -o $@
-\tcp ../js/${func_name}_proxy.js ../release/
 clean:
 \t@rm -f *.o $(TARGET)
+\trm -rf ../release/*
 .PHONY:clean`
 
 }
