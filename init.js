7f718811bc80ad977907c4c72b8c997786fb2ba1
wangli(李望) <wangli@tencent.com>
2019-03-25 15:49:09 +0800

    c 通信代码 实现代码 初始化代码 分离

--- init.js
+++ init.js
@@ -36,7 +36,7 @@ async function mkdir(dir) {
     });
 }
 
-const version = '0.0.10';
+const version = '0.0.11';
 
 async function main() {
     program
@@ -62,16 +62,22 @@ async function main() {
 
     const js_c_code = generate_js(func_name, req_params, rsp_params, init_params, desc, version, 'c');
     const js_python_code = generate_js(func_name, req_params, rsp_params, init_params, desc, version, 'python');
-    const c_code = generate_c(func_name, req_params, rsp_params, init_params, desc, version);
+    const {
+        c_imp_code,
+        c_init_code,
+        c_header_code,
+        c_pipe_code,
+     } = generate_c(func_name, req_params, rsp_params, init_params, desc, version);
     const python_code = generate_python(func_name, req_params, rsp_params, init_params, desc, version);
     const makefile_code = generate_makefile(func_name);
-
     await writeFile(`./build/jscallc/${func_name}_proxy.js`, js_c_code);
-    await writeFile(`./build/c/${func_name}_imp.cpp`, c_code);
+    await writeFile(`./build/c/${func_name}_imp.cpp`, c_imp_code);
+    await writeFile(`./build/c/${func_name}_init.cpp`, c_init_code);
+    await writeFile(`./build/c/${func_name}_header.h`, c_header_code);
+    await writeFile(`./build/c/${func_name}_pipe.cpp`, c_pipe_code);
     await writeFile(`./build/jscallpython/${func_name}_proxy.js`, js_python_code);
     await writeFile(`./build/python/${func_name}_imp.py`, python_code);
     await writeFile(`./build/c/makefile`, makefile_code);
-    
 }
 
 main();
